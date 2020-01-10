2d4de517063094d82c979f7bd48948115eabaea0
Jani Niculescu <1701388@keskusrh.scp.fi>
2020-01-10 09:40:50 +0200

    test

---
+++ Landmarks-Jnicu/Landmarks-Jnicu/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks-Jnicu
+//
+//  Created by Jani Niculescu on 10/01/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
